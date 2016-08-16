f6a414b71d7f7cfcdf2f1ed97acad98e4bcd2de0
Jason Fry <Jason@FryMini.local>
2016-08-16 17:04:27 -0400

    Added new index.html, main.js, and style.css files to create a Memory Game website.

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,8 @@
 console.log("JS file is connected to HTML! Woo!")
+
+//declaring variables for 4 cards
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
