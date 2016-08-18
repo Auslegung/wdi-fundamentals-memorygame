d3653c7b21151f67a8e0210f6d6032e3669ecf3b
Jason Fry <jmphry@gmail.com>
2016-08-18 09:43:58 -0400

    Added createBoard function

--- starter-code/main.js
+++ starter-code/main.js
@@ -6,9 +6,23 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardOne ===cardThree) {
+var numOfCards = 4;
+
+/* if (cardOne ===cardThree) {
 	alert( "You found a match!" );
 }
 	else {
 		alert( "Sorry, try again.");
-	}
+	} */
+
+var gameBoard = document.getElementById("game-board");
+
+function createBoard() {
+	for (var i = 0; i < numOfCards; i += 1) {
+		var card = 	document.createElement("div");
+		card.className = "card";
+		/*document.getElementById("game-board")*/gameBoard.appendChild(card);
+	} // end for loop
+} // end createBoard function
+
+createBoard();
