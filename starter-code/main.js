6a0e33a5ef87df2ab7f0b49b924cd03054a2dcbd
Jason Fry <Jason@FryMini.local>
2016-08-17 20:56:52 -0400

    Alert if cardOne and cardThree match

--- starter-code/main.js
+++ starter-code/main.js
@@ -6,3 +6,9 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+if (cardOne ===cardThree) {
+	alert( "You found a match!" );
+}
+	else {
+		alert( "Sorry, try again.");
+	}
